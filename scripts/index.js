9d29f55bcaa3e30ac7895af4a5d4b26ed2c97b3c
AndreySHB <deerhunter9136@gmail.com>
2025-04-05 18:20:33 +0300

    Здравствуйте, пожалуйста посмотрите мое задание

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardsContainer = document.querySelector('.places__list');
+const onDelButListener = (event) => {
+    if (event.target.closest('.card__delete-button')) {
+        event.target.closest('li').remove();
+    }
+};
+initialCards.forEach((cardData) => {
+    const card = createCard(cardData, onDelButListener);
+    cardsContainer.append(card);
+});
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function createCard(cardData, delButListener) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImg = cardElement.querySelector('.card__image');
+    cardImg.src = cardData.link;
+    cardImg.alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__delete-button')
+        .addEventListener(
+            'click', delButListener
+        );
+    return cardElement;
+}
